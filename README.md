378ac6e36aa9fc5d9469c297bafde5e28162c47d
Mike Smith <grimbough@gmail.com>
2018-04-20 19:31:52 +0200

    Created plot and added github numbers

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# denbi_reporting
+# de.NBI Reporting and KPIs
+
 Automating reporting to de.NBI
+
+# Number of support questions for selected HD-HuB tools
+
+![](http://www.huber.embl.de/users/msmith/denbi/support_questions.png)
--- support_stats.R
+++ support_stats.R
@@ -2,67 +2,148 @@ library(httr)
 library(stringr)
 library(dplyr)
 library(lubridate)
+library(ggplot2)
 
-getBiocPostByAPI <- function(post_id) {
-    post_url <- paste0('https://support.bioconductor.org/api/post/', post_id)
+getPostByAPI <- function(site, post_id) {
+    
+    url_root <- switch(tolower(site), 
+                       bioc = 'https://support.bioconductor.org',
+                       biostars = 'https://www.biostars.org',
+                       NULL)
+    if(is.null(url_root)) { stop('Unknown site') }
+    
+    post_url <- paste0(url_root, '/api/post/', post_id)
     res <- content(httr::GET(url = post_url))
-    return(as_tibble(res[ c('creation_date', 'id', 'root_id', 'tag_val', 'title') ]))
+    return(as_tibble(res[ c('creation_date', 'id', 'root_id', 'title') ]))
 }
 
-getBiostarsPostByAPI <- function(post_id) {
-    post_url <- paste0('https://www.biostars.org/api/post/', post_id)
-    res <- content(httr::GET(url = post_url))
-    return(as_tibble(res[ c('creation_date', 'id', 'root_id', 'tag_val', 'title') ]))
-}
-
-
-getPostsByTag <- function(site = "bioc", tag = "biomaRt", pages = 10) {
-    post_ids <- lapply(seq_len(pages), FUN = function(x) {
+getPostsByTag <- function(site = "bioc", tag = "biomaRt", n_pages = 10) {
+    
+    post_ids <- lapply(seq_len(n_pages), FUN = function(x) {
         
         url_root <- switch(tolower(site), 
                            bioc = 'https://support.bioconductor.org',
                            biostars = 'https://www.biostars.org',
-                           )
+                           NULL)
+        
+        if(is.null(url_root)) { stop('Unknown site') }
         
         url <- paste0(url_root, '/t/', tag, '/',
                       '?sort=Creation&limit=All%20time&answered=all&page=', x)
         
-        page <- readLines(url)
+        page <- httr::GET(url)
         
-        post_lines <- page[grep(page, pattern = 'post-title')+2]
-        
-        ids <- str_match(post_lines, '/p/([0-9]+)/')[,2]
-        ids <- ids[ which(!is.na(ids)) ]
-        ids
+        ## only carry on if we get a good page
+        if(page$status_code != 200) {
+            return(NULL)
+        } else {
+            
+            tmp <- tempfile()
+            writeLines(content(page, as = "text"), con = tmp)
+            page <- readLines(con = tmp)
+            
+            post_lines <- page[grep(page, pattern = 'post-title')+2]
+            
+            if(!length(post_lines)) {
+                return(NULL)
+            } else {
+                ids <- str_match(post_lines, '/p/([0-9]+)/')[,2]
+                ids <- ids[ which(!is.na(ids)) ]
+                ids
+            }
+        }
     })
     
-    all_posts <- lapply(unlist(post_ids), getBiocPostByAPI) %>%
+    if(is.null(unlist(post_ids))) {
+        return(NULL)
+    } else {
+    all_posts <- lapply(unlist(post_ids), getPostByAPI, site = site) %>%
         bind_rows() %>%
-        mutate(creation_date = as.Date(creation_date))
+        mutate(creation_date = as.Date(creation_date), 
+               site = site,
+               tag = tag)
+        return(all_posts)
+    }
     
 }
 
-getPostsByUser <- function(user_id = '3986') {
+getPostsByUser <- function(site = 'bioc', user_id = '3986', n_pages = 20) {
     
-    post_ids <- lapply(1:20, FUN = function(x) {
-    url <- paste0('https://support.bioconductor.org/u/', user_id, '/',
-                  '?sort=Creation&limit=All%20time&answered=all&page=', x)
-    
-    page <- readLines(url)
-    post_lines <- page[grep(page, pattern = 'post-title')+4]
-    
-    ids <- str_match(post_lines, '/p/[0-9]+/#([0-9]+)')[,2]
-    ids <- ids[ which(!is.na(ids)) ]
-    ids
+    post_ids <- lapply(seq_len(n_pages), FUN = function(x) {
+        
+        url_root <- switch(tolower(site), 
+                           bioc = 'https://support.bioconductor.org',
+                           biostars = 'https://www.biostars.org',
+                           NULL)
+        
+        url <- paste0(url_root, '/u/', user_id, '/',
+                      '?sort=Creation&limit=All%20time&answered=all&page=', x)
+        
+        page <- httr::GET(url)
+        
+        ## only carry on if we get a good page
+        if(page$status_code != 200) {
+            return(NULL)
+        } else {
+            
+            tmp <- tempfile()
+            write_html(content(page), tmp)
+            page <- readLines(tmp)
+            
+            post_lines <- page[grep(page, pattern = 'post-title')+4]
+            
+            if(!length(post_lines)) {
+                return(NULL)
+            } else {
+                
+                ids <- str_match(post_lines, '/p/[0-9]+/#([0-9]+)')[,2]
+                ids <- ids[ which(!is.na(ids)) ]
+                return(ids)
+            }
+        }
     })
     
-    all_posts <- lapply(unlist(post_ids), getBiocPostByAPI) %>%
+    if(is.null(post_ids)) {
+        return(NULL)
+    } else {
+    all_posts <- lapply(unlist(post_ids), getPostByAPI, site = site) %>%
         bind_rows() %>%
-        mutate(creation_date = as.Date(creation_date))
+        mutate(creation_date = as.Date(creation_date), site = site)
+    }
     
 }
 
 
-posts.biomart <- getPostsByTag()
+getGithubIssues <- function(repo = "rhdf5") {
+    tmp <- content(GET('https://api.github.com/repos/grimbough/rhdf5/issues?state=all'))
+    tmp2 <- lapply(tmp, function(x) return(tibble(creation_date = as.Date(x$created_at))))
+    
+    res <- tibble(creation_date = pull(bind_rows(tmp2), creation_date), tag = repo, site = "github")
+    return(res)
+}
+
+posts.biomart <- bind_rows(getPostsByTag(tag = 'biomaRt', site = "bioc"), 
+                           getPostsByTag(tag = 'biomaRt', site = 'biostars'),
+                           getPostsByTag(tag = 'rhdf5', site = "bioc", n_pages = 4), 
+                           getPostsByTag(tag = 'rhdf5', site = 'biostars', n_pages = 1),
+                           getPostsByTag(tag = 'IONiseR', site = "bioc", n_pages = 3), 
+                           getPostsByTag(tag = 'IONiseR', site = 'biostars', n_pages = 1))
+
+posts.biomart %>% 
+    bind_rows(posts.biomart, tmp3) %>%
+    filter(year(creation_date) >= 2015) %>% 
+    ggplot(aes(fill = site, x = year(creation_date))) + 
+    geom_bar() +
+    scale_fill_manual(values = c('#1a81c2', '#8f2c47', 'grey'),
+                      name = "Site",
+                      labels = c("bioconductor.org", "biostars.org", "github.com")) + 
+    theme_bw() + 
+    xlab('Year') +
+    facet_wrap(~ tag) 
+    
+
+
 posts.rhdf5 <- getPostsByTag(tag = 'rhdf5', pages = 3)
-posts.me <- getPostsByUser()
+
+posts.me <- getPostsByUser(site = 'bioc', user_id = '3986', n_pages = 20)
+posts.me2 <- getPostsByUser(site = 'biostars', user_id = '4156', n_pages = 5)
