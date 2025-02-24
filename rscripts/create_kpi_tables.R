cf1dfa263e5c0f44ce15998134f884199fe22863
Mike Smith <grimbough@gmail.com>
2025-02-24 17:26:56 +0100

    Move things around

--- rscripts/create_kpi_tables.R
+++ rscripts/create_kpi_tables.R
@@ -1,6 +1,16 @@
-source('support_stats.R')
+source('get_conda_stats.R')
+
+if (!require("pak", quietly = TRUE))
+  install.packages("pak")
+
+packages_required <- c('lubridate', 'BiocPkgTools', 'dplyr', 'openxlsx')
+
+pak::pkg_install(packages_required)
+
+library(lubridate)
 library(BiocPkgTools)
 library(dplyr)
+library(openxlsx)
 
 deNBI_start_year <- 2015
 current_year <- year(now())
@@ -39,37 +49,30 @@ packages <- dplyr::tribble(
   'progeny', TRUE,
   'limix', FALSE)
 
-## these are the packages Sina has asked for
-pkgs_required_today <- c(
-  'biomaRt',
-  'DEXSeq',
-  'DESeq2',
-  'BiocWorkflowTools',
-  'rnaseqGene',
-  'rhdf5+Rhdf5lib',
-  'EBImage',
-  'IHW',
-  'SIAMCAT',
-  'r-circlize',
-  'ComplexHeatmap',
-  'EnrichedHeatmap',
-  'rGREAT',
-  'HilbertCurve',
-  'gtrellis',
-  'MOFA+MOFA2',
-  'slalom',
-  'limix'
-)
+## If Sina has asked for a specific subset of packages list them here
+## Otherwise leave this at NULL
+pkgs_required_today <- NULL
+# pkgs_required_today <- c(
+#   'biomaRt',
+#   'DEXSeq',
+#   'DESeq2',
+#   'BiocWorkflowTools'
+# )
 
-fct_levels <- packages %>% 
-  mutate(PackageComb = if_else(grepl(pkg, pattern = 'hdf5'), 'rhdf5+Rhdf5lib', pkg)) %>%
-  mutate(PackageComb = if_else(grepl(PackageComb, pattern = 'MOFA'), 'MOFA+MOFA2', PackageComb)) %>%
-  magrittr::extract2("PackageComb") %>% unique()
 
+## Get the download counts.  
+## First two are for Bioconductor directly and for Bioconductor packages obtained from bioconda
+## These tables initially have all Bioconductor packages and will be filtered later for the packages we actually want.
+## Final table is for non-bioconductor packages
 tab <- BiocPkgTools::biocDownloadStats()
 tab2 <- BiocPkgTools::anacondaDownloadStats()
 tab3 <- getCondaStats(filter(packages, in_bioc == FALSE) %>% pull(pkg), bioc = FALSE)
 
+## Do some wrangling to the three tables so they are in the same format
+## We filter() for the relevant date range and packages,
+## then select() only the columns we need for our report tables
+## The mutate() lines merge together the counts for some tools that are very heavily intertwined.
+## e.g. Rhdf5lib and rhdf5 will always be used together
 bioc_stats <- tab %>%
   filter(Package %in% packages$pkg, Year >= deNBI_start_year, !is.na(Date)) %>%
   select(-Year, -Month, -pkgType) %>%
@@ -88,35 +91,39 @@ conda_stats_notbioc <- tab3 %>%
   mutate(PackageComb = Package) %>%
   mutate(Nb_of_distinct_IPs = Nb_of_downloads)
 
-## combine the two sets of anaconda statistics into a single table
-conda_stats <- bind_rows(conda_stats_bioc, conda_stats_notbioc)
+## combine the three sets of statistics into a single table
+all_stats <- bind_rows(bioc_stats, conda_stats_bioc, conda_stats_notbioc)
 
-
-dl_stats <- bind_rows(bioc_stats, conda_stats) %>% 
+## Now we combine the counts for the various sources. 
+##  We group by each package and month, then simply sum the download counts.
+dl_stats <- all_stats %>% 
   group_by(PackageComb, Date) %>% 
   summarise(Nb_of_downloads = sum(Nb_of_downloads)) %>%
   ungroup() %>%
   mutate(Month = month(Date), Year = year(Date)) %>%
   select(PackageComb, Year, Month, Nb_of_downloads)
 
+## Split our single big table into a list of smaller tables, one for each tool
 dl_list <- split(dl_stats, dl_stats$PackageComb)
 
 
+## Final data prettying.
 ## on each package specific table
-## - rename the column, 
+## - rename the column to something more descriptive, 
 ## - select only the years we're interest in
-## - remove merge name column - name is now in the overall list
+## - remove merged name column - name is now in the overall list
 dl_list <- lapply(dl_list, function(x) {
   
   x %>% 
     rename("Monthly_Downloads" = Nb_of_downloads) %>%
-    filter(Year >= 2018, Year < 2025) %>%
+    filter(Year >= 2018, Year <= current_year) %>%
     select(-PackageComb)
   
 })
 
 ## include only the packages we need in the report for Sina
-dl_list <- dl_list[ pkgs_required_today ]
+if(!is.null(pkgs_required_today))
+  dl_list <- dl_list[ pkgs_required_today ]
 
-ss <- gs4_create(spring("Huber Tool KPIs - Feb %i", current_year), sheets = dl_list)
-
+#ss <- gs4_create(spring("Huber Tool KPIs - Feb %i", current_year), sheets = dl_list)
+write.xlsx(dl_list, file = "tool_KPIs.xlsx")
---
+++ rscripts/get_conda_stats.R
@@ -0,0 +1,19 @@
+getCondaStats <- function(packages = 'rhdf5', bioc = TRUE) {
+  
+  temp_file <- tempfile()
+  url <- ifelse (bioc, 
+                 'https://github.com/grimbough/anaconda-download-stats/raw/master/rdata/bioc_counts.rds',
+                 'https://github.com/grimbough/anaconda-download-stats/raw/master/rdata/all_counts.rds'
+  )  
+  download.file(url, destfile = temp_file)
+  
+  tab <- readRDS(temp_file)
+  if(bioc != TRUE) {
+    tab <- tab %>% rename(Package = pkg_name, Year = year, 
+                          Month = month, Nb_of_downloads = counts)
+  }
+  tab %>% 
+    dplyr::filter(Package %in% packages) %>%
+    mutate(Date = ymd(paste(Year, Month, '01', sep = '-')))
+}
+
